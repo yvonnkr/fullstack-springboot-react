04191e1c8ffc2a955c975e160c965441f173c770
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-04 13:16:47 +0000

    Initial commit

---
+++ src/test/java/com/yvolabs/fullstack/MainTests.java
@@ -0,0 +1,13 @@
+package com.yvolabs.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
