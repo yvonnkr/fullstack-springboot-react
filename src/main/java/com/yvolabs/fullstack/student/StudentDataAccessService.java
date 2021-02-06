96055b4b51f48def79e5b3ce57db3841f4e9a8bf
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-06 14:12:44 +0000

    JDBC template, selectAllStudents & row mappers

--- src/main/java/com/yvolabs/fullstack/student/StudentDataAccessService.java
+++ src/main/java/com/yvolabs/fullstack/student/StudentDataAccessService.java
@@ -1,21 +1,50 @@
 package com.yvolabs.fullstack.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
-import static com.yvolabs.fullstack.student.Student.Gender.FEMALE;
-import static com.yvolabs.fullstack.student.Student.Gender.MALE;
-
 @Repository
 public class StudentDataAccessService {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public StudentDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public List<Student> selectAllStudents(){
-        return List.of(
-                new Student(UUID.randomUUID(),"Johnxxxxxyyyy","Doe", "johndoe@gmail.com", MALE),
-                new Student(UUID.randomUUID(),"Jane","Doe", "janedoe@gmail.com", FEMALE),
-                new Student(UUID.randomUUID(),"Steve","Smith", "stevesmith@gmail.com", MALE),
-                new Student(UUID.randomUUID(),"Mary","Smith", "marysmith@gmail.com", FEMALE)
-        );
+        String sql = String.format("SELECT  student_id,  first_name,  last_name,  email,  gender FROM student");
+
+        List<Student> students = jdbcTemplate.query(sql, mapStudentFromDb());
+
+        return students;
+
+    }
+
+    private RowMapper<Student> mapStudentFromDb() {
+        return (resultSet, i) -> {
+            String studentIdStr = resultSet.getString("student_id");
+            UUID studentId = UUID.fromString(studentIdStr);
+
+            String firstName = resultSet.getString("first_name");
+            String lastName = resultSet.getString("last_name");
+            String email = resultSet.getString("email");
+
+            String genderStr = resultSet.getString("gender").toUpperCase();
+            Student.Gender gender = Student.Gender.valueOf(genderStr);
+
+            return new Student(
+                    studentId,
+                    firstName,
+                    lastName,
+                    email,
+                    gender
+            );
+        };
     }
 }
